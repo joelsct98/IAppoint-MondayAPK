50199a74efa4cc27420ee52147fbc5adde0b9c81
juankarmjackson <juankarmjgs@gmail.com>
2023-06-21 00:04:11 +0200

    feat: Monday Login

--- src/App.js
+++ src/App.js
@@ -27,56 +27,10 @@ const App = () => {
     setIsLoading(true);
     monday.setToken(tokenValue);
 
-    monday.api(`mutation {
-      create_board(
-        board_name: "Julex",
-        board_kind: public,
-        description: "Bienvenido a Nuestro sistema de Ventas de Julex.ia"
-      ) {
-        id
-      }
-    }`).then(response => {
-      const newBoardId = response.data.create_board.id;
-      setBoardData({ id: newBoardId });
-      setInputValue(newBoardId);
-      setIsLoading(false);
-      fetchData(newBoardId);
-      createNewColumns(newBoardId); // Crear las columnas automáticamente después de crear el board
-    }).catch(error => {
-      console.log("Error al crear el board:", error);
-      setIsLoading(false);
-    });
-  }
+    // Código para crear el board y obtener los datos
+    // ...
 
-  const fetchData = (boardId) => {
-    setIsLoading(true);
-    monday.api(`query {
-      boards(ids: ${boardId}) {
-        name
-        columns {
-          id
-          title
-          settings_str
-        }
-        items {
-          id
-          name
-          column_values {
-            id
-            value
-            title
-          }
-        }
-      }
-    }`).then(response => {
-      const board = response.data.boards[0];
-      setBoardData(board);
-      setIsLoading(false);
-      console.log(board);
-    }).catch(error => {
-      console.log(error);
-      setIsLoading(false);
-    });
+    setIsLoading(false);
   }
 
   const handleSubmit = (event) => {
@@ -89,119 +43,21 @@ const App = () => {
     setTokenInput(event.target.value);
   }
 
-  const createNewColumns = async (newBoardId) => {
-    try {
-      const createColumnPromises = [
-        monday.api(`mutation {
-          create_column(
-            board_id: ${newBoardId},
-            title: "Teléfono",
-            column_type: phone
-          ) {
-            id
-          }
-        }`),
-        monday.api(`mutation {
-          create_column(
-            board_id: ${newBoardId},
-            title: "Email",
-            column_type: email
-          ) {
-            id
-          }
-        }`),
-        monday.api(`mutation {
-          create_column(
-            board_id: ${newBoardId},
-            title: "Julex.ai",
-            column_type: status
-          ) {
-            id
-          }
-        }`),
-        monday.api(`mutation {
-          create_column(
-            board_id: ${newBoardId},
-            title: "Activar Julex.ai",
-            column_type: button
-          ) {
-            id
-          }
-        }`)
-      ];
-  
-      const responses = await Promise.all(createColumnPromises);
-  
-      const column1Id = responses[0].data.create_column.id;
-      const column2Id = responses[1].data.create_column.id;
-      const column3Id = responses[2].data.create_column.id;
-      const column4Id = responses[3].data.create_column.id;
-  
-      console.log("Nueva columna Teléfono creada:", column1Id);
-      console.log("Nueva columna Email creada:", column2Id);
-      console.log("Nueva columna Julex.ai creada:", column3Id);
-      console.log("Nueva columna Activar Julex.ai creada:", column4Id);
-    } catch (error) {
-      console.log("Error al crear las columnas:", error);
-    }
-  };
+  const fetchData = (boardId) => {
+    setIsLoading(true);
+
+    // Código para obtener los datos del board
+    // ...
+
+    setIsLoading(false);
+  }
 
   return (
     <div className="capa">
       <div className="popup">
         <img src={julexImage} alt="Julex" />
         <h1>Julex.ia</h1>
-        {/* <LoginForm /> */}
-        {isLoading ? (
-          <p>Cargando...</p>
-        ) : (
-          <div>
-            {boardData && (
-              <div>
-                <h2>Datos del board:</h2>
-                <form onSubmit={handleSubmit}>
-                  <label htmlFor="boardId">Board ID:</label>
-                  <input
-                    type="number"
-                    id="boardId"
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                  />
-                  <label htmlFor="token">Token:</label>
-                  <input
-                    type="text"
-                    id="token"
-                    value={tokenInput}
-                    onChange={handleTokenChange}
-                  />
-                  <br></br>
-                  <button type="submit">Actualizar</button>
-                </form>
-                <h2>Board: {boardData.name}</h2>
-                <div className="datos">
-                  <div className="dato-columna">
-                    <p>Nombre:</p>
-                    {boardData.items.map(item => (
-                      <p key={item.id}>{item.name}</p>
-                    ))}
-                  </div>
-                  <div className="dato-columna">
-                    <p>Email:</p>
-                    {boardData.items.map(item => (
-                      <p key={item.id}>{item.name}</p>
-                    ))}
-                  </div>
-                  <div className="dato-columna">
-                    <p>Julex:</p>
-                    {boardData.items.map(item => (
-                      <p key={item.id}>{item.name}</p>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-        )}
+        <LoginForm></LoginForm>
       </div>
     </div>
   );
